af8b5780ffb9d44f6a04b2f2a55e0107a130ce73
swastik <swastiksuman@gmail.com>
2018-08-13 23:32:20 +0530

    enum string in db

--- src/main/java/com/authoring/springhibernateaudit/entities/Course.java
+++ src/main/java/com/authoring/springhibernateaudit/entities/Course.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
@@ -28,6 +30,7 @@ public class Course implements Serializable {
 	@Column(nullable = false)
 	private String name;
 	
+	@Enumerated(EnumType.STRING)
 	@Column
 	private Difficulty difficulty;
 	
